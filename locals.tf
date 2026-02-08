1223ca433fb02d4883f6912542055366194da811
armdupre <a-joseph.dupre@keysight.com>
2026-02-08 17:06:23 -0500

    Initial Checkin.

---
+++ locals.tf
@@ -0,0 +1,32 @@
+locals {
+	CanIpForward = true
+	Eth0PrivateIpAddress = var.Eth0PrivateIpAddress
+	Eth0PublicIpAddressName = "${local.Preamble}-eth0-ip-addr"
+	Eth0SubnetName = var.Eth0SubnetName
+	Eth0VpcNetworkName = var.Eth0VpcNetworkName
+	Eth1PrivateIpAddress = var.Eth1PrivateIpAddress
+	Eth1PrivateIpAliases = var.Eth1PrivateIpAliases
+	Eth1SubnetName = var.Eth1SubnetName
+	Eth1VpcNetworkName = var.Eth1VpcNetworkName
+	Eth2PrivateIpAddress = var.Eth2PrivateIpAddress
+	Eth2PrivateIpAliases = var.Eth2PrivateIpAliases
+	Eth2SubnetName = var.Eth2SubnetName
+	Eth2VpcNetworkName = var.Eth2VpcNetworkName
+	InstanceId = var.InstanceId
+	InstanceName = "${local.Preamble}-instance"
+	MachineType = var.MachineType
+	MarketplaceImageName = var.MarketplaceImageName
+	MarketplaceImageProjectId = var.MarketplaceImageProjectId
+	NetworkTargetTags = var.NetworkTargetTags
+	Preamble = replace(replace("${local.UserLoginTag}-${local.UserProjectTag}-${local.Tag}-${local.Version}-${local.InstanceId}", "_", "-"), ".", "-")
+	RegionName = var.RegionName
+	SerialPortEnable = true
+	SleepDelay = var.SleepDelay
+	Tag = var.Tag
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+	Version = var.Version
+	ZoneName = var.ZoneName
+	init_cli = base64gzip(var.init_cli)
+}
